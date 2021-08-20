498f1059568889d18f87dd4b63576a1ecf962811
tomoki maekawa <maekawatomoki@team-lab.com>
2021-08-20 13:37:05 +0900

    add s3+cloudfront

--- lib/static-site-cdk-stack.ts
+++ lib/static-site-cdk-stack.ts
@@ -1,9 +1,58 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3deploy from '@aws-cdk/aws-s3-deployment';
+import * as cloudfront from '@aws-cdk/aws-cloudfront'
+import * as iam from '@aws-cdk/aws-iam';
+
+export interface StaticSiteProps {
+  bucketName: string
+}
 
 export class StaticSiteCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.Construct, id: string, props?: StaticSiteProps) {
+    super(scope, id);
 
-    // The code that defines your stack goes here
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, 'cloudfront-OAI', {
+      comment: `OAI for ${id}`
+    });
+
+    const siteBucket = new s3.Bucket(this, 'StaticSiteBucket', {
+      bucketName: props?.bucketName,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html',
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      versioned: true
+    });    
+    siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [siteBucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+    new cdk.CfnOutput(this, 'Bucket', { value: siteBucket.bucketName });
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: siteBucket,
+            originAccessIdentity: cloudfrontOAI
+          },
+          behaviors: [{
+            isDefaultBehavior: true,
+            compress: true,
+            allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+          }],
+        }
+      ]
+    });
+    new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId })
+    
+    new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [s3deploy.Source.asset('./site-contents')],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ['/*']
+    });
   }
 }
